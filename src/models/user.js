9e72216e46600bdc6db37a06db9b183544d7809d
Vu Minh Dao (FA.G0) <DaoVM1@fsoft.com.vn>
2019-01-30 10:27:17 +0700

    Sua UserRoute 30-1

--- src/models/user.js
+++ src/models/user.js
@@ -1,30 +1,22 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const autoIncrement = require('mongoose-auto-increment');
-var friend = new Schema({
-    userName: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    imgUrl: {
-        type: String,
-    }
-})
+
 var userSchema = new Schema({
     userName: {
         type: String,
         required: true,
         unique: true
     },
-    password: {
-        type: String,
-        required: true
-    },
+    name:{
+        type:String,
+        required:true
+    }
+    ,
     imgUrl: {
         type: String,
     },
-    listFriends :[friend]
+    listFriends :[]
 })
 
 const User = mongoose.model('User', userSchema);
--- src/route/userroute.js
+++ src/route/userroute.js
@@ -11,28 +11,77 @@ module.exports = function (app) {
                 res.status(400).send(e);
             })
         });
+    // get User By Id
+    app.route('/getUserById/:id')
+        .get(function (req, res) {
+            var query = {
+                _id: req.params.id
+            };
+            User.findById(query,
+                (err, doc) => {
+                    if (err) {
+                        res.send("có lỗi " + err)
+                    }
+                    res.send(doc)
+                }
+            )
+        });
     //thêm user
     app.route('/createUser')
         .post(function (req, res) {
             var user = new User({
                 userName: req.body.userName,
-                password: req.body.password,
-                imgUrl: req.body.imgUrl
+                imgUrl: req.body.imgUrl,
+                name: req.body.name
             })
-            user.save().then((user) => {
-                res.send(user)
-            }, (e) => {
-                res.send("có lỗi " + e)
-            })
+            var firstTime = false;
+            // kiểm tra xem user đã đăng nhập lần nào chưa?
+            User.find({
+                userName: user.userName
+            }).exec(function (e, raw) {
+                if (e) {
+                    console.log("có lỗi")
+                    //res.send(e)
+                }
+                if (raw.length !== 0) {
+                    res.send(raw)
+                } else {
+                    user.save().then((user) => {
+                        res.send(user)
+                    }, (e) => {
+                        res.send("có lỗi " + e)
+                    })
+                }
+            });
+
+
+
         });
-    //update user
-    app.route('/updateUserByUserName')
+    //update  name user
+    app.route('/updateNameByUserName')
         .put(function (req, res) {
             var query = {
                 userName: req.body.userName
             };
             User.findOneAndUpdate(query, {
-                password: req.body.password,
+                name: req.body.name,
+            },
+                { new: true },
+                (err, doc) => {
+                    if (err) {
+                        res.send("có lỗi khi update" + err)
+                    }
+                    res.send(doc)
+                }
+            )
+        });
+    //update avatar user
+    app.route('/updateAvatarByUserName')
+        .put(function (req, res) {
+            var query = {
+                userName: req.body.userName
+            };
+            User.findOneAndUpdate(query, {
                 imgUrl: req.body.imgUrl
             },
                 { new: true },
@@ -58,6 +107,61 @@ module.exports = function (app) {
                     res.send(raw);
                 });
         });
-
-
+    // add friend
+    app.route('/addFriendByUserName')
+        .put(function (req, res) {
+            var query = {
+                userName: req.body.userName
+            };
+            var friend = {
+                _id: req.body.idFriend,
+                name: req.body.name,
+                imgUrl: req.body.imgUrl
+            }
+            User.findOneAndUpdate(query,
+                { $push: { listFriends: friend } },
+                { new: true },//phải thêm ông nội này mới chạy đước nhá
+                (e, raw) => {
+                    if (e) {
+                        res.status(400).send('Invalid user');
+                        //res.send(e)
+                    }
+                    res.send(raw);
+                })
+        })
+    // dell friend
+    app.route('/delFriendByUserNameAndIdFriend')
+        .put(function (req, res) {
+            var query = {
+                userName: req.body.userName
+            };
+            var friend = {
+                _id: req.body.friendId
+            }
+            User.findOneAndUpdate(query,
+                { $pull: { listFriends: friend } },
+                { new: true },//phải thêm ông nội này mới chạy đước nhá
+                (e, raw) => {
+                    if (e) {
+                        res.status(400).send('Invalid user');
+                        //res.send(e)
+                    }
+                    res.send(raw);
+                })
+        })
+    // search people 
+    app.route('/searchPeople/:userName')
+        .get(function (req, res) {
+            User.find({
+                userName: {
+                    $ne: req.params.userName
+                }
+            }).exec(function (e, raw) {
+                if (e) {
+                    res.status(400).send('Invalid user');
+                    //res.send(e)
+                }
+                res.send(raw);
+            });
+        });
 }
